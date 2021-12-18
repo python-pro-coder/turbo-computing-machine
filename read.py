d32b2a6ae2c5449a9d4f1107b5a2a205f2febb8c
python-pro-coder <hardikagarwal0902@gmail.com>
2021-12-18 15:50:12 +0000

    initial commit

---
+++ read.py
@@ -0,0 +1,2 @@
+f=open("/workspace/turbo-computing-machine/textfile.txt","r+")
+print(f.tell())
--- sample.py
+++
@@ -1,6 +0,0 @@
-print("use of assert statement")
-def negativecheck(number):
-    assert(number>=0), "OOPS... NEGATIVE NUMBER"
-    print(number*number)
-print (negativecheck(100))
-print (negativecheck(-350))
